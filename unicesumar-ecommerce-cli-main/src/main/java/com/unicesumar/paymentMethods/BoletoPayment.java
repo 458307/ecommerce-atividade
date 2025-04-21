0df167961913ea1690693289f57c134782c33f25
Matheus Felipe Campioto Catenacci <matheus.campioto04@outlook.com>
2025-04-20 22:48:07 -0300

    Update BoletoPayment.java

--- unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/paymentMethods/BoletoPayment.java
+++ unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/paymentMethods/BoletoPayment.java
@@ -3,6 +3,6 @@ package com.unicesumar.paymentMethods;
 public class BoletoPayment implements PaymentMethod {
     @Override
     public void pay(double amount) {
-        System.out.println("Pagamento efetuado com sucesso via boleto");
+        System.out.println("Pagamento confirmado com sucesso via boleto");
     }
 }

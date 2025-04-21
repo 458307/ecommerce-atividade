7b87d83e7d41627962d0e51a033d08cfba4cfef6
Matheus Felipe Campioto Catenacci <matheus.campioto04@outlook.com>
2025-04-20 22:48:42 -0300

    Update PaymentType.java

--- unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/paymentMethods/PaymentType.java
+++ unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/paymentMethods/PaymentType.java
@@ -1,5 +1,17 @@
 package com.unicesumar.paymentMethods;
 
 public enum PaymentType {
-    PIX, BOLETO, CARTAO;
+    PIX("PIX"),
+    BOLETO("Boleto"),
+    CARTAO("Cartão de Crédito");
+
+    private final String descricao;
+
+    PaymentType(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }

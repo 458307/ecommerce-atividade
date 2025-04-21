5608c03d9b76d9354822663020929410ccd4f2f8
Matheus Felipe Campioto Catenacci <matheus.campioto04@outlook.com>
2025-04-20 22:48:21 -0300

    Update CreditCardPayment.java

--- unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/paymentMethods/CreditCardPayment.java
+++ unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/paymentMethods/CreditCardPayment.java
@@ -3,6 +3,6 @@ package com.unicesumar.paymentMethods;
 public class CreditCardPayment implements PaymentMethod {
     @Override
     public void pay(double amount) {
-        System.out.println("Pagamento efetuado com sucesso via cartão de crédito");
+        System.out.println("Pagamento confirmado com sucesso via cartão de crédito");
     }
 }

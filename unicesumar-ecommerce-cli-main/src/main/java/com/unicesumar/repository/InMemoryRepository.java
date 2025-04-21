13b43187e35b2ee73bfd2f69ae421130a649fe98
Matheus Felipe Campioto Catenacci <matheus.campioto04@outlook.com>
2025-04-20 22:51:14 -0300

    Update InMemoryRepository.java

--- unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/repository/InMemoryRepository.java
+++ unicesumar-ecommerce-cli-main/src/main/java/com/unicesumar/repository/InMemoryRepository.java
@@ -1,24 +1,22 @@
 package com.unicesumar.repository;
 
+import com.unicesumar.entities.Entity;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.UUID;
-
-import com.unicesumar.entities.Entity;
 
 public class InMemoryRepository<T extends Entity> implements EntityRepository<T> {
     private ArrayList<T> dataStore = new ArrayList<>();
 
     @Override
     public void save(T entity) {
-        this.dataStore.removeIf(e -> e.getUuid().equals(entity.getUuid()));
+        this.dataStore.removeIf(e -> e.getId() == entity.getId());
         this.dataStore.add(entity);
     }
 
     @Override
-    public Optional<T> findById(UUID id) {
-        return this.dataStore.stream().filter(e -> e.getUuid().equals(id)).findFirst();
+    public Optional<T> findById(int id) {
+        return this.dataStore.stream().filter(e -> e.getId() == id).findFirst();
     }
 
     @Override
@@ -27,7 +25,7 @@ public class InMemoryRepository<T extends Entity> implements EntityRepository<T>
     }
 
     @Override
-    public void deleteById(UUID id) {
-        this.dataStore.removeIf(e -> e.getUuid().equals(id));
+    public void deleteById(int id) {
+        this.dataStore.removeIf(e -> e.getId() == id);
     }
-}
+} 
